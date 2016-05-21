ccf115c3cf1f383376f6469d30ba98679e8b902c
Danny Peng <ibxccc123@gmail.com>
2016-05-21 19:26:28 -0400

    Update StackPairDP.h
    Updated readability through indentations.

--- StackPairDP.h
+++ StackPairDP.h
@@ -8,75 +8,75 @@ using namespace std;
 template<class T>
 class StackPair{
 
-int frontStack, rearStack, upperBound;
-vector<T>* elements; 
+  int frontStack, rearStack, upperBound;
+  vector<T>* elements; 
 
-public:
-StackPair(int guaranteedCapacity);
-~StackPair();
-int size() const;
-bool frontEmpty() const;
-bool rearEmpty() const;
-T popFront();
-T popRear();
-void pushFront(T& newItem);
-void pushRear(T& newItem);
-};
+  public:
+  StackPair(int guaranteedCapacity);
+  ~StackPair();
+  int size() const;
+  bool frontEmpty() const;
+  bool rearEmpty() const;
+  T popFront();
+  T popRear();
+  void pushFront(T& newItem);
+  void pushRear(T& newItem);
+  };
 
 template<class T>
-StackPair<T>::StackPair(int guaranteedCapacity) : frontStack(-1), rearStack(guaranteedCapacity), 
+  StackPair<T>::StackPair(int guaranteedCapacity) : frontStack(-1), rearStack(guaranteedCapacity), 
                                                    upperBound(guaranteedCapacity), elements(new 
                                                    vector<T>(guaranteedCapacity)) {}
 
 template<class T>
 StackPair<T>::~StackPair(){
-delete(elements);
+  delete(elements);
 }
 
 template<class T>
 int StackPair<T>::size() const{
-return (frontStack + 1) +  (upperBound-rearStack);
+  return (frontStack + 1) +  (upperBound-rearStack);
 } 
 
 template<class T>
 bool StackPair<T>::frontEmpty() const{
-return frontStack == -1;
+  return frontStack == -1;
 }
 
 template<class T>
 bool StackPair<T>::rearEmpty() const{
-return rearStack == upperBound;
+  return rearStack == upperBound;
 }
 
 template<class T>
 T StackPair<T>::popFront(){
-if(!frontEmpty()){
-return elements->at(frontStack--);
-}
-else{
-cerr << "Define an exception later for this.";
-return ' ';
-}
+  if(!frontEmpty()){
+    return elements->at(frontStack--);
+  }
+  else{
+    cerr << "Define an exception later for this.";
+    return ' ';
+  }
 }
 
 template<class T>
 T StackPair<T>::popRear(){
-if(!rearEmpty()){
-return elements->at(rearStack++);
-}
-else{
-cerr << "Define an exception later for this.";
-return ' ';
-}
+  if(!rearEmpty()){
+    return elements->at(rearStack++);
+  }
+  else{
+    cerr << "Define an exception later for this.";
+  return ' ';
+  }
 }
 //REQ: Vector must not be completely full to prevent out-of-range errors.
 template<class T>
 void StackPair<T>::pushFront(T& newItem){
-elements->at(++frontStack) = newItem;
+  elements->at(++frontStack) = newItem;
 }
 //REQ: Vector must not be completely full to prevent out-of-range errors.
 template<class T>
 void StackPair<T>::pushRear(T& newItem){
-elements->at(--rearStack) = newItem;
+  elements->at(--rearStack) = newItem;
 }
 #endif
